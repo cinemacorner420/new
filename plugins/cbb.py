9b9f45e100a73cc2e056d8386dca4c50c405578b
cinemacorner420 <84710033+cinemacorner420@users.noreply.github.com>
2022-03-18 23:00:09 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>TOM CRUISE</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ No Bio : @HCLinksPLAYit ⚡</b>",
+            text = f"<b>○ NO BIO 😜</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
